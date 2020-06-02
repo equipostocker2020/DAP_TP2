d5b48f29e84adccfcff65a451bedb4a717ceb2f3
gonzalo <equipostocker@gmail.com>
2020-06-02 15:13:53 -0300

    ver y borrar

--- delete.php
+++ delete.php
@@ -5,7 +5,7 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
     require_once "config.php";
     
     // Preparando la query para eliminar
-    $sql = "DELETE FROM employees WHERE id = ?";
+    $sql = "DELETE FROM TAREAS WHERE ID_TAREA = ?";
     
     if($stmt = mysqli_prepare($link, $sql)){
         // Bind a la variable.
--- read.php
+++ read.php
@@ -1,50 +1,51 @@
 <?php
-// Check existence of id parameter before processing further
+// Chequea que exista el parametro id.
 if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
     // Include config file
     require_once "config.php";
     
-    // Prepare a select statement
-    $sql = "SELECT * FROM employees WHERE id = ?";
+    // Sentencia Select
+    $sql = "SELECT * FROM TAREAS WHERE ID_TAREA = ?";
     
     if($stmt = mysqli_prepare($link, $sql)){
-        // Bind variables to the prepared statement as parameters
+        // Bind de variables para setearlas como parámetros
         mysqli_stmt_bind_param($stmt, "i", $param_id);
         
-        // Set parameters
+        // Setea parámetros
         $param_id = trim($_GET["id"]);
         
-        // Attempt to execute the prepared statement
+        // Intento de ejecución
         if(mysqli_stmt_execute($stmt)){
             $result = mysqli_stmt_get_result($stmt);
     
             if(mysqli_num_rows($result) == 1){
-                /* Fetch result row as an associative array. Since the result set
-                contains only one row, we don't need to use while loop */
+                /* El resultado lo arma como un array. Como el resultado tiene una sola columna
+                no se necesita un loop */
                 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
                 
-                // Retrieve individual field value
-                $name = $row["name"];
-                $address = $row["address"];
-                $salary = $row["salary"];
+                // Recupera el valor de cada campo
+                $fecha = $row["FECHA_ASIGNACION"];
+                $descripcion = $row["DESCRIPCION"];
+                $tiempo = $row["TIEMPO_ASIGNADO"];
+                $observaciones = $row["OBSERVACIONES"];
             } else{
-                // URL doesn't contain valid id parameter. Redirect to error page
+                // Si la URL no encuentra el ID redirecciona a la página de error.
                 header("location: error.php");
                 exit();
             }
             
         } else{
-            echo "Oops! Something went wrong. Please try again later.";
+            echo "Oops! Algo salió mal.";
         }
     }
      
-    // Close statement
+    // Se cierra elk statement
     mysqli_stmt_close($stmt);
     
-    // Close connection
+    // Se cierra la conexión
     mysqli_close($link);
 } else{
-    // URL doesn't contain id parameter. Redirect to error page
+    // Si la URL no encuentra el ID redirecciona a la página de error.
     header("location: error.php");
     exit();
 }
@@ -70,19 +71,23 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
             <div class="row">
                 <div class="col-md-12">
                     <div class="page-header">
-                        <h1 class="titulo text-center">Ver Empleado</h1>
+                        <h1 class="titulo text-center">Ver Tarea</h1>
                     </div>
                     <div class="form-group">
-                        <label>Nombre</label>
-                        <p class="form-control-static"><?php echo $row["name"]; ?></p>
+                        <label>Fecha de asignación</label>
+                        <p class="form-control-static"><?php echo $row["FECHA_ASIGNACION"]; ?></p>
                     </div>
                     <div class="form-group">
-                        <label>Dirección</label>
-                        <p class="form-control-static"><?php echo $row["address"]; ?></p>
+                        <label>Descripción de la tarea<./label>
+                        <p class="form-control-static"><?php echo $row["DESCRIPCION"]; ?></p>
                     </div>
                     <div class="form-group">
-                        <label>Sueldo</label>
-                        <p class="form-control-static"><?php echo $row["salary"]; ?></p>
+                        <label>Tiempo asignado a la tarea.</label>
+                        <p class="form-control-static"><?php echo $row["TIEMPO_ASIGNADO"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Observaciones.</label>
+                        <p class="form-control-static"><?php echo $row["OBSERVACIONES"]; ?></p>
                     </div>
                     <p><a href="index.php" class="btn btn-primary">Volver</a></p>
                 </div>
